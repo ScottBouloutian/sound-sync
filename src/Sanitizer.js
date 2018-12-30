7d1134af2464897569c61e1ed7ff62ca39e52f5e
Scott Bouloutian <ScottBouloutian@gmail.com>
2018-12-30 14:13:24 -0500

    remove emoji from filenames

--- src/Sanitizer.js
+++ src/Sanitizer.js
@@ -1,3 +1,10 @@
-const getFilename = track => track.title.replace(/[<>:"/\\|*?]/g, '-');
+const emojiRegex = require('emoji-regex');
+
+const regex = emojiRegex();
+const getFilename = track => (
+    track.title
+        .replace(/[<>:"/\\|*?]/g, '-')
+        .replace(regex, '')
+);
 
 module.exports = { getFilename };
